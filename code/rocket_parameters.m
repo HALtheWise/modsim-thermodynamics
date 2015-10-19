e3cac560227d78da052bb0a81fa2440a093b720f
Eric Miller <eric@legoaces.org>
2015-10-19 11:19:48 -0400

    Made up numbers in rocket_parameters

--- code/rocket_parameters.m
+++ code/rocket_parameters.m
@@ -2,10 +2,9 @@
 
 p.air_temp = 290;
 
-p.fuel_heat_capacity = ;
 p.fuel_specific_heat = 15000; % paper
 p.fuel_cold_temp = 91.7; % K, paper
-p.fuel_volume = ;
+p.fuel_volume = 0.84 * pi; % WAG
 p.fuel_density = 71.0; % paper
 
 p.metal_specific_heat = 450; % paper (copper)
@@ -19,6 +18,6 @@ p.tubing_surface_area = 8 * pi;
 
 p.heat_transfer_coefficient = 3000; %complete guess
 
-p.heat_flow_from_exhaust = ;
+p.heat_flow_from_exhaust = 1e6; %J/s WAG
 
-p.fuel_flow_rate = ; %m^3/s
+p.fuel_flow_rate = .2; %m^3/s WAG
