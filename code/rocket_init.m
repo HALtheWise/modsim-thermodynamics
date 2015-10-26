35ad6e079a1313bd2d123283e0f43b58f83653c7
unknown <emiller@BX4PT32.olin.edu>
2015-10-26 10:43:37 -0400

    Switched to ode23s

--- code/rocket_init.m
+++ code/rocket_init.m
@@ -6,14 +6,16 @@ initial_temperature = p.air_temp; % K
 % Calculate initial energy
 metal_heat = p.metal_density*p.metal_volume * p.metal_specific_heat * p.metal_initial_temp;
 total_fuel_heat = p.fuel_density*p.fuel_volume * p.fuel_specific_heat * p.fuel_cold_temp;
+
 stocks = metal_heat;
 stocks(1,2:p.num_coolant_stocks+1) = total_fuel_heat / p.num_coolant_stocks;
 disp(stocks)
 
 % Run simulation
 time_span = [0 1000];
-[Times, Stocks] = ode45(@rocket_flows, time_span, stocks, [], p);
+[Times, Stocks] = ode23s(@rocket_flows, time_span, stocks, [], p);
 
+% Convert energies to temperatures
 for i=1:size(Stocks)
     Stocks(i, 1) = energy_to_temp(Stocks(i, 1), p.metal_specific_heat * p.metal_volume * p.metal_density);
     Stocks(i, 2:end) = energy_to_temp(Stocks(i, 2:end), p.fuel_specific_heat * p.fuel_volume * p.fuel_density / p.num_coolant_stocks);
