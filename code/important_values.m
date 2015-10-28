adc2610a8b3eaa3d9c117a381de6249c0a54bd66
Eric Miller <eric@legoaces.org>
2015-10-28 10:54:03 -0400

    Updated important_values

--- code/important_values.m
+++ code/important_values.m
@@ -1,6 +1,4 @@
-function [res1, res2] = important_values(Stocks)
-    max_metal_temp = max(Stocks(:,1));
-    max_fuel_temp = max(Stocks(:,length(Stocks)));
-    res1 = max_metal_temp;
-    res2 = max_fuel_temp;
+function [max_metal_temp, max_fuel_temp] = important_values(Stocks)
+    max_metal_temp = max(Stocks(:,end/2));
+    max_fuel_temp = max(Stocks(:,end));
 end
